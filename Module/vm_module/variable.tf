f0a5d11be46e3f5d994b708ab6cab51879afa8f4
FILIZA1 <113948897+FILIZA1@users.noreply.github.com>
2023-02-05 17:04:56 -0500

    Add files via upload

---
+++ Module/vm_module/variable.tf
@@ -0,0 +1,25 @@
+variable "vm_name" {
+
+}
+
+variable "resource_group_name" {
+
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "username" {
+
+}
+
+variable "password" {
+
+}
+
+variable "subnet_id" {
+
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "rg_name" {
+  default = "rg1"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "vnet_name" {
+  default = "vnet"
+}
+
+variable "subnet1_name" {
+  default = "subnet1"
+}
+
+variable "subnet2_name" {
+  default = "subnet2"
+}
+
+variable "rg2_name" {
+  default = "rg2"
+}
+
